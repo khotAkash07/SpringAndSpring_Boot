47a5c6373f1d37c2cdb6d301a095aa9ab043eda8
khotAkash07 <khotakash5737@gmail.com>
2025-12-31 16:19:01 +0530

    Spring Practice

--- DiOperations/src/main/java/org/akash/ClientApp.java
+++ DiOperations/src/main/java/org/akash/ClientApp.java
@@ -9,17 +9,23 @@ public class ClientApp {
 
 		try {
 			ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("web.xml");
-			Player p = (Player) context.getBean("player");
-			// p.show();
-
-			Student_01 stud = (Student_01) context.getBean("student");
-			// stud.show();
-
-			Student_01 stud2 = (Student_01) context.getBean("student1");
-			// stud2.show();
-
-			ListExmple listt = (ListExmple) context.getBean("l");
-			listt.showList();
+//			Player p = (Player) context.getBean("player");
+//			p.show();
+//
+//			Student_01 stud = (Student_01) context.getBean("student");
+//			stud.show();
+//
+//			Student_01 stud2 = (Student_01) context.getBean("student1");
+//			stud2.show();
+//
+//			ListExmple listt = (ListExmple) context.getBean("l");
+//			listt.showList();
+//			
+//			MapDIApp md = (MapDIApp) context.getBean("m");
+//			md.show();
+			
+			Student_01 s1 = (Student_01) context.getBean("s");
+			s1.show();
 		} catch (BeansException e) {
 			e.printStackTrace();
 		}
--- DiOperations/src/main/java/org/akash/Student_01.java
+++ DiOperations/src/main/java/org/akash/Student_01.java
@@ -5,7 +5,12 @@ public class Student_01 {
 	private String name;
 	private int marks;
 	private int m;
-
+	private Player p;
+	
+	public void setP(Player p) {
+		this.p = p;
+	}
+	public Student_01() {}
 	public Student_01(long id, String name, int marks) {
 		this.id = id;
 		this.name = name;
@@ -23,5 +28,6 @@ public class Student_01 {
 		// System.out.println(id + "\t" + name + "\t" + marks);
 		System.out.println("\n");
 		System.out.println(id + "\t" + name + "\t" + marks + "\t" + m);
+		System.out.print(p.getId()+"\t"+p.getName()+"\t"+p.getRun());
 	}
 }
