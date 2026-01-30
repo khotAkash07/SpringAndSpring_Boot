d1d331ca6d96c621ba4e0a6cfef12c80d7cdcd7a
khotAkash07 <khotakash5737@gmail.com>
2026-01-30 09:34:00 +0530

    SpringPractice

--- src/main/java/org/demo/models/Car.java
+++ src/main/java/org/demo/models/Car.java
@@ -13,6 +13,7 @@ public class Car {
         String color = "red";
 
         System.out.println("Color: "+ color);
+
         String type = "4Wheeler";
         System.out.println("Type: "+ type);
     }
